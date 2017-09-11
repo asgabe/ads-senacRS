5a711ecd998e3741aecc7fad49ab9c604172f573
Gabriel Corrêa <gbl.correa8@gmail.com>
2017-09-11 20:40:08 -0300

    [+] material of class

---
+++ algorithms-and-programming-II/APS01/src/Dishes.java
@@ -0,0 +1,12 @@
+
+public class Dishes {
+	private String name;
+	private String description;
+	private double price;
+
+	public Dishes(String name, String description, double price) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+}
