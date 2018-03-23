bf88f9da92d4c59b7ce06086032e30cff3e4fe33
Gabriel Corrêa <gabe.crs@gmail.com>
2018-03-23 16:08:57 -0300

    fixed gulpfile

--- human-computer-interface/project-calendar/gulpfile.js
+++ human-computer-interface/project-calendar/gulpfile.js
@@ -1,78 +1,23 @@
 'use strict';
-
-/* GULP PLUGINS
-========================================================================== */
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	cleanCSS = require('gulp-clean-css'),
-	prefix = require('gulp-autoprefixer'),
-	rename = require('gulp-rename'),
-	sourcemaps = require('gulp-sourcemaps'),
-	clean = require('gulp-clean'),
-	requirejsOptimize = require('gulp-requirejs-optimize'),
-	runSequence = require('run-sequence');
-
-
-/* DEV TASKS
-========================================================================== */
-
-//Compile the Sass css, prefix the styles and create the sourceMap for the dev CSS
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+ 
 gulp.task('css', function() {
-
-	return gulp.src('dev/sass/major.scss')
-	.pipe(sourcemaps.init())
+	return gulp.src('src/sass/major.scss')
 	.pipe(sass({ errLogToConsole:true }))
-	.pipe(prefix('last 15 version'))
-	.pipe(rename('major.css'))
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('dev/css'));
-
+	.pipe(gulp.dest('dist/css'));
 });
 
-/* PROD TASKS
-========================================================================== */
-
-//Minify the dev CSS for production
-gulp.task('minifyCSS', function () {
-
-	return gulp.src('dev/sass/major.scss')
-	.pipe(sass({ errLogToConsole:true }))
-	.pipe(prefix('last 15 version'))
-	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(rename('major.css'))
-	.pipe(gulp.dest('assets/css'));
-
-});
-
-//Use RequireJS to concatenate the js assets for production
-gulp.task('scripts', function () {
-	return gulp.src('dev/js/main.js').pipe(gulp.dest('assets/js'));
-});
-
-//Clean the assets folder which will receive the rev'ed assets
-gulp.task('clean', function () {
-    return gulp.src('assets', {read: false})
-    .pipe(clean());
-});
-
-/* DEV TASKS
-========================================================================== */
-//Creates a livereload Server and build static assets on the fly.
 gulp.task('default', function() {
+	//Star the liveReload Server
+	livereload({ start: true });
 
 	//When a SASS file change, build the CSS
-	gulp.watch('dev/sass/**/*.scss', ['css'])
+	gulp.watch('src/sass/**/*.scss', ['css']);	
 
+	gulp.watch(['src/sass/**/*.scss', '**/*.html', 'src/js/**/*.js']).on('change', function(file) {
+		livereload.changed(file.path);
+	});
 });
-
-//Build the complete site inside the "build" folder
-gulp.task('build', function() {
-
- 	runSequence('clean', 'minifyCSS', 'scripts');
-
-});
-
-
-
-
-
